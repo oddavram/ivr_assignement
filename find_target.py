0c9a879f37b26e5021e18ec8e0cbde1285defca2
hollymay <hb14475@my.bristol.ac.uk>
2020-11-26 04:40:34 +0000

    Create find_target.py

---
+++ find_target.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python3
+
+import roslib
+import sys
+import rospy
+import cv2
+import numpy as np
+from std_msgs.msg import String
+from sensor_msgs.msg import Image
+from std_msgs.msg import Float64MultiArray, Float64
+from cv_bridge import CvBridge, CvBridgeError
+
+
+class find_target:
+
+  # Defines publisher and subscriber
+  def __init__(self):
+    # initialize the node named image_processing
+    rospy.init_node('image_processing', anonymous=True)
+    # initialize a publisher to send images from camera1 to a topic named image_topic1
+    # self.image_sub1 = rospy.Subscriber("image_topic1",Image, queue_size = 1)
+    # self.image_sub2 = rospy.Subscriber("image_topic1",Image, queue_size = 1)
+
+    # initialize a subscriber to recieve messages rom a topic named /robot/camera1/image_raw and use callback function to recieve data
+    self.image_sub1 = rospy.Subscriber("/camera1/robot/image_raw",Image,self.callback1)
+    self.image_sub2 = rospy.Subscriber("/camera2/robot/image_raw",Image,self.callback2)
+
+    # initialize the bridge between openCV and ROS
+    self.bridge = CvBridge()
+
+
+# Recieve data from camera 1, process it, and publish
+  def callback1(self,data):
+    # Recieve the image
+    try:
+      cv_image1 = self.bridge.imgmsg_to_cv2(data, "bgr8")
+    except CvBridgeError as e:
+      print(e)
+
+    square_image1 = self.detect_square(cv_image1)
+    print("square_image1")
+    print(square_image1)
+
+
+  # Recieve data from camera 1, process it, and publish
+  def callback2(self,data):
+    # Recieve the image
+    try:
+      cv_image2 = self.bridge.imgmsg_to_cv2(data, "bgr8")
+      # self.cv_image1 = self.bridge.imgmsg_to_cv2(data, "bgr8")
+      # print("publishing")
+      # print(self.bridge.imgmsg_to_cv2(data, "bgr8"))
+    except CvBridgeError as e:
+      print(e)
+
+    square_image2 = self.detect_square(cv_image2)
+    print("square_image2")
+    print(square_image2)
+
+
+
+  def detect_square(self, image):
+    
+    mask = cv2.inRange(image, (0, 50, 50), (50, 155, 255))
+    
+    cnts = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    # cnts = imutils.grab_contours(cnts)
+    print(len(cnts))
+    cnts = cnts[0]
+    # print(cnts)
+    for c in cnts:
+      M = cv2.moments(c)
+      cx = int(M['m10'] / M['m00'])
+      cy = int(M['m01'] / M['m00'])
+      shape = self.detect_shape(c)
+    
+
+      if shape == "Square":
+        # print("shape")
+        # print(shape)
+        return np.array([cx, cy])
+
+    # return np.array([cx, cy])
+
+
+  def detect_shape(self, c):
+    shape = "Unidentified"
+    peri = cv2.arcLength(c, True)
+    approx = cv2.approxPolyDP(c, 0.04*peri, True)
+    if len(approx) == 4:
+        shape = "Square"
+    else:
+        shape = "Circle"
+    return shape
+
+
+    
+
+# call the class
+def main(args):
+  ic = find_target()
+  try:
+    rospy.spin()
+    # find_target()
+  except KeyboardInterrupt:
+    print("Shutting down")
+  cv2.destroyAllWindows()
+
+# run the code if the node is called
+if _name_ == '_main_':
+    main(sys.argv)
